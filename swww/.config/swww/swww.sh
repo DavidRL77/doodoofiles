d72089dc65ad100478c6308e596660a55f690b6a
davicro <david.rollonla@gmail.com>
2026-01-30 20:28:39 +0100

    Made swww initial transitions slower

--- swww/.config/swww/swww.sh
+++ swww/.config/swww/swww.sh
@@ -4,6 +4,6 @@ wallpaper_dir=~/.cache/wallpaper
 
 swww clear
 sleep 0.05
-swww img $wallpaper_dir/current_wallpaper_grayscale --transition-type fade --transition-duration 1 --transition-fps 60
+swww img $wallpaper_dir/current_wallpaper_grayscale --transition-type fade --transition-duration 2 --transition-fps 60
 sleep 1
-swww img $wallpaper_dir/current_wallpaper  --transition-type grow  --transition-pos top-left --transition-duration 1 --transition-fps 60
+swww img $wallpaper_dir/current_wallpaper  --transition-type grow  --transition-pos top-left --transition-duration 2 --transition-fps 60
