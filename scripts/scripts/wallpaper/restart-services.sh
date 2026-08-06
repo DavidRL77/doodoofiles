5d0a1480623b0928a8aabb76fe3e36cb47dfa0af
davicro <david.rollonla@gmail.com>
2026-08-06 18:39:57 +0200

    Made restarting services faster in set-wallpaper

--- scripts/scripts/wallpaper/restart-services.sh
+++ scripts/scripts/wallpaper/restart-services.sh
@@ -2,12 +2,14 @@
 
 # Separate script to restart only the necessary services (add/remove as needed)
 
-systemctl --user restart waybar
-pkill -USR1 cava
+systemctl --user restart waybar&
+pkill -USR1 cava&
 
 # For some reason I need to send this to kitty as well when
 # cava is running on a terminal since it won't reload the 
 # colors automatically.
-pkill -USR1 kitty
+pkill -USR1 kitty&
 
-pywalfox update
+pywalfox update&
+makoctl reload&
+hyprctl reload&
