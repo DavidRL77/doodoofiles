95b016435458c8aecd98cdace3be48df055720c1
davicro <david.rollonla@gmail.com>
2026-08-13 17:26:47 +0200

    Added another thing that doesn't work

--- hyprland/.config/hypr/scripts/swww-init.sh
+++ hyprland/.config/hypr/scripts/swww-init.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 awww-daemon --no-cache &
-sleep 1
-source ~/.config/swww/swww.sh
+
+# wait until the daemon is properly initialized
+until awww query >/dev/null 2>&1 
+do 
+    sleep 0.05 
+done
+
+~/.config/swww/swww.sh
