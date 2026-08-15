3742f8821e078c0de5ea05fd14df583f392a0e01
davicro <david.rollonla@gmail.com>
2026-08-15 23:03:37 +0200

    Fix deprecated functions in swayimg

--- swayimg/.config/swayimg/init.lua
+++ swayimg/.config/swayimg/init.lua
@@ -3,24 +3,18 @@ function exit()
 end
 
 function toggleText()
-	if swayimg.text.visible() then
-		swayimg.text.hide()
-	else
-		swayimg.text.show()
-		swayimg.text.set_timeout(0)
-	end
+	swayimg.text.visible = swayimg.text.visible
 end
 
 function zoom(amount)
-	swayimg.viewer.set_abs_scale(swayimg.viewer.get_scale()+amount)
+	swayimg.viewer.set_abs_scale(swayimg.viewer.scale+amount)
 end
 
 -- General
-swayimg.enable_overlay(true)
-swayimg.viewer.set_window_background(0x00000000)
-swayimg.gallery.set_window_color(0x00000000)
-swayimg.text.set_foreground(0xffffffff)
-swayimg.text.hide();
+swayimg.overlay = true
+swayimg.gallery.window_color = 0x00000000
+swayimg.text.color = 0xffffffff
+swayimg.text.visible = false
 
 -- Info viewer position
 swayimg.viewer.set_text("topleft", {
@@ -30,8 +24,8 @@ swayimg.viewer.set_text("topleft", {
 -- Key bindings
 swayimg.viewer.on_key("q", exit)
 swayimg.viewer.on_key("i", toggleText)
-swayimg.viewer.on_key("Ctrl-Left", function() swayimg.viewer.switch_image("prev") end)
-swayimg.viewer.on_key("Ctrl-Right", function() swayimg.viewer.switch_image("next") end)
+swayimg.viewer.on_key("Ctrl-Left", function() swayimg.viewer.open("prev") end)
+swayimg.viewer.on_key("Ctrl-Right", function() swayimg.viewer.open("next") end)
 swayimg.viewer.on_key("Ctrl-Up", function() zoom(.1) end)
 swayimg.viewer.on_key("Ctrl-Down", function() zoom(-.1) end)
 swayimg.viewer.on_key("r", function() swayimg.viewer.rotate(90) end)
