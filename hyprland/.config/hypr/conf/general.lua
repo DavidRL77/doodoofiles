de3b3039583b53afcde570f3d649f53510741ade
davicro <david.rollonla@gmail.com>
2026-08-19 09:19:20 +0200

    Disable splash rendering in hyprland

--- hyprland/.config/hypr/conf/general.lua
+++ hyprland/.config/hypr/conf/general.lua
@@ -3,7 +3,8 @@ hl.config({
         force_default_wallpaper = 0,
         disable_hyprland_logo = true,
         mouse_move_enables_dpms = true,
-        key_press_enables_dpms = true
+        key_press_enables_dpms = true,
+        disable_splash_rendering = true
     },
 
     dwindle = {
