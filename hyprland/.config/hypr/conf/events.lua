fef5a9a94e797c3151ec4292183b103c4ffa308b
davicro <david.rollonla@gmail.com>
2026-08-24 22:15:44 +0200

    Added wallpaper reload on monitor added

--- hyprland/.config/hypr/conf/events.lua
+++ hyprland/.config/hypr/conf/events.lua
@@ -4,3 +4,7 @@ hl.on("hyprland.start", function ()
     hl.exec_cmd("hyprpm reload")
     hl.exec_cmd(Globals.hyprScripts.."bluetooth-trusted.sh")
 end)
+
+hl.on("monitor.added", function ()
+    hl.exec_cmd("systemctl --user restart awww-daemon")
+end)
