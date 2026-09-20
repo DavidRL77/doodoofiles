0bed6a9003b35837f3586c80ad15184b4b14ea11
davicro <david.rollonla@gmail.com>
2026-09-20 17:50:21 +0200

    Swayimg: open in gallery mode when more than one image is opened
    Also adjusts thumbnail size to fit the screen better (currently sucks)

--- swayimg/.config/swayimg/init.lua
+++ swayimg/.config/swayimg/init.lua
@@ -3,12 +3,40 @@ function exit()
 end
 
 function toggleText()
-	swayimg.text.visible = swayimg.text.visible
+	swayimg.text.visible = !swayimg.text.visible
 end
 
 function zoom(amount)
 	swayimg.viewer.set_abs_scale(swayimg.viewer.scale+amount)
 end
+
+function clamp(n, min, max)
+	if n < min then
+		return min
+	elseif n > max then
+		return max
+	else
+		return n
+	end
+end
+
+function adjust_thumb_size()
+	local size = swayimg.get_window_size()
+	local padding = 0
+	local area = (size.width - padding) * (size.height - padding)
+	local n = swayimg.imagelist.size
+	local thumb_size = tonumber(string.format("%.f", (area/n)^0.5))
+	swayimg.gallery.thumb_size = clamp(thumb_size,200,800)
+end
+
+-- Open in gallery mode if more than one image is opened
+swayimg.on_initialized(function()
+	if swayimg.imagelist.size > 1 then
+		swayimg.mode="gallery"
+	end
+
+	adjust_thumb_size()
+end)
 
 -- General
 swayimg.overlay = true
@@ -16,9 +44,12 @@ swayimg.gallery.window_color = 0x00000000
 swayimg.viewer.set_window_background(0x00000000)
 swayimg.text.color = 0xffffffff
 swayimg.text.visible = false
-
--- Info viewer position
+swayimg.text.size = 16
+swayimg.gallery.padding_size=10
 swayimg.viewer.text = {
+	["topleft"]={"{name}"}
+}
+swayimg.gallery.text = {
 	["topleft"]={"{name}"}
 }
 
@@ -29,6 +60,8 @@ swayimg.viewer.on_key("Ctrl-Left", function() swayimg.viewer.open("prev") end)
 swayimg.viewer.on_key("Ctrl-Right", function() swayimg.viewer.open("next") end)
 swayimg.viewer.on_key("Ctrl-Up", function() zoom(.1) end)
 swayimg.viewer.on_key("Ctrl-Down", function() zoom(-.1) end)
+swayimg.viewer.on_key("Ctrl-plus", function() zoom(.1) end)
+swayimg.viewer.on_key("Ctrl-minus", function() zoom(-.1) end)
 swayimg.viewer.on_key("r", function() swayimg.viewer.rotate(90) end)
 swayimg.viewer.on_key("Space", function() swayimg.viewer.reset() end)
 
